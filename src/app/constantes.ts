eb6384163e070fe323223f937c100d2dbdecd342
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-05-13 21:24:53 -0500

    implementacion de seccion de proyectos

--- src/app/constantes.ts
+++ src/app/constantes.ts
@@ -1,4 +1,7 @@
 export const SITE_TITLE = "CedeSystem";
+export const URL_PAYPHONE = "https://ppls.me/21v4efHcwLwTYh4JYffKw";
+export const URL_PEIGO = "https://ppls.me/21v4efHcwLwTYh4JYffKw";
+export const URL_PAYPAL = "https://ppls.me/21v4efHcwLwTYh4JYffKw";
 
 
 
